320f26d6fda222cdfe836eb59b95b68317240e3c
Ted Hunter <tedh@slalom.com>
2021-10-07 13:16:21 -0400

    [wip] working router status data source

--- google/data_source_google_compute_router_status.go
+++ google/data_source_google_compute_router_status.go
@@ -2,6 +2,7 @@ package google
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"google.golang.org/api/compute/v1"
@@ -11,22 +12,23 @@ func dataSourceGoogleComputeRouterStatus() *schema.Resource {
 	routeElemSchema := datasourceSchemaFromResourceSchema(resourceComputeRoute().Schema)
 
 	return &schema.Resource{
-		Read: dataSourceComputeRouterRead,
+		Read: dataSourceComputeRouterStatusRead,
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:        schema.TypeString,
-				Description: "Name of the router to query.",
+				Description: "Name of the router to query. This can be the router name, or a fully qualified self-link.",
 				Required:    true,
+				Computed:    false,
 			},
 			"project": {
 				Type:        schema.TypeString,
 				Description: "Project ID of the target router.",
-				Required:    false,
+				Optional:    true,
 			},
 			"region": {
 				Type:        schema.TypeString,
 				Description: "Region of the target router.",
-				Required:    false,
+				Optional:    true,
 			},
 			"network": {
 				Type:        schema.TypeString,
@@ -36,16 +38,20 @@ func dataSourceGoogleComputeRouterStatus() *schema.Resource {
 			"best_routes": {
 				Type:        schema.TypeList,
 				Description: "Best routes for this router's network.",
-				Elem:        routeElemSchema,
-				Computed:    true,
-				Required:    false,
+				Elem: &schema.Resource{
+					Schema: routeElemSchema,
+				},
+				Computed: true,
+				Required: false,
 			},
 			"best_routes_for_router": {
 				Type:        schema.TypeList,
 				Description: "Best routes learned by this router.",
-				Elem:        routeElemSchema,
-				Computed:    true,
-				Required:    false,
+				Elem: &schema.Resource{
+					Schema: routeElemSchema,
+				},
+				Computed: true,
+				Required: false,
 			},
 		},
 	}
@@ -58,17 +64,22 @@ func dataSourceComputeRouterStatusRead(d *schema.ResourceData, meta interface{})
 		return err
 	}
 
-	region := config.Region
-	if r, ok := d.GetOk("region"); ok {
-		region = r.(string)
-	}
-
 	project, err := getProject(d, config)
 	if err != nil {
 		return err
 	}
 
-	resp, err := config.NewComputeClient(userAgent).Routers.GetRouterStatus(project, region, d.Id()).Do()
+	region, err := getRegion(d, config)
+	if err != nil {
+		return err
+	}
+
+	var name string
+	if n, ok := d.GetOk("name"); ok {
+		name = n.(string)
+	}
+
+	resp, err := config.NewComputeClient(userAgent).Routers.GetRouterStatus(project, region, name).Do()
 	if err != nil {
 		return err
 	}
@@ -87,13 +98,16 @@ func dataSourceComputeRouterStatusRead(d *schema.ResourceData, meta interface{})
 		return fmt.Errorf("Error setting best_routes_for_router: %s", err)
 	}
 
+	d.SetId(fmt.Sprintf("projects/%s/regions/%s/routers/%s", project, region, name))
+
 	return nil
 }
 
 func mapRoutes(d *schema.ResourceData, field string, routes []*compute.Route) error {
+	log.Printf("[DEBUG] mapping %d routes for %s", len(routes), field)
 	results := make([]map[string]interface{}, len(routes))
 
-	for _, route := range routes {
+	for i, route := range routes {
 		output := make(map[string]interface{})
 		output["dest_range"] = route.DestRange
 		output["name"] = route.Name
@@ -106,6 +120,7 @@ func mapRoutes(d *schema.ResourceData, field string, routes []*compute.Route) er
 		output["priority"] = route.Priority
 		output["tags"] = route.Tags
 		output["next_hop_network"] = route.NextHopNetwork
+		results[i] = output
 	}
 
 	return d.Set(field, results)
